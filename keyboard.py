5f0cdf8c9de1c3e8e914c24154b7f061daa33fc9
abdullohdev2012 <139839913+abdullohdev2012@users.noreply.github.com>
2023-08-01 20:59:16 +0500

    privet

---
+++ keyboard.py
@@ -0,0 +1,19 @@
+
+from aiogram.types import  InlineKeyboardMarkup, InlineKeyboardButton
+
+inline1 = InlineKeyboardMarkup (inline_keyboard=[
+    [
+        InlineKeyboardButton(text="1",callback_data="n1"),
+        InlineKeyboardButton(text="2",callback_data="n2"),
+        InlineKeyboardButton(text="3",callback_data="n3"),
+        InlineKeyboardButton(text="4",callback_data="n4"),
+        InlineKeyboardButton(text="5",callback_data="n5"),
+        InlineKeyboardButton(text="6",callback_data="n6"),
+        InlineKeyboardButton(text="7",callback_data="n7"),
+        InlineKeyboardButton(text="8",callback_data="n8"),
+        InlineKeyboardButton(text="9",callback_data="n9"),
+        InlineKeyboardButton(text="10",callback_data="n10"),
+    ],
+]
+
+)
